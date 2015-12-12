efef19c87a1739d31ecc017f093b816e7dfe091f
rostun <rosa.m.tung@gmail.com>
2015-12-12 16:56:25 -0500

    teeny weeny controls

--- Assets/playerControl.cs
+++ Assets/playerControl.cs
@@ -2,14 +2,33 @@
 using System.Collections;
 
 public class playerControl : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown("a"))
+		{
+			print("Left Key");
+			transform.Translate(Vector3.left*Time.deltaTime);
+			
+		}
+		else if(Input.GetKeyDown("d"))
+		{
+			print("Right Key");
+			transform.Translate(Vector3.right*Time.deltaTime);
+		}
+		else if(Input.GetKeyDown("s"))
+		{
+			print("push off");
+			transform.Translate(Vector3.up*Time.deltaTime);
+		}
+		if(Input.GetKeyDown("space"))
+		{
+			print("shoot");
+		}
 	}
 }
