cb27bcc5135c56d3c7e8b27649146b928480c578
rostun <rosa.m.tung@gmail.com>
2016-03-29 11:51:31 -0400

    ASDF
    ASDF

--- Assets/playerControl.cs
+++ Assets/playerControl.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class playerControl : MonoBehaviour {
 	
+	public GameObject bullet = null;
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -10,25 +12,27 @@ public class playerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		//move circle around
 		if(Input.GetKeyDown("a"))
 		{
 			print("Left Key");
-			transform.Translate(Vector3.left*Time.deltaTime);
+			transform.Translate(Vector3.left*Time.deltaTime*10);
 			
 		}
 		else if(Input.GetKeyDown("d"))
 		{
 			print("Right Key");
-			transform.Translate(Vector3.right*Time.deltaTime);
+			transform.Translate(Vector3.right*Time.deltaTime*10);
 		}
-		else if(Input.GetKeyDown("s"))
-		{
-			print("push off");
-			transform.Translate(Vector3.up*Time.deltaTime);
-		}
-		if(Input.GetKeyDown("space"))
-		{
-			print("shoot");
+		//shoot
+		else if (Input.GetMouseButtonDown(1)){
+			print("fire coordinates: " + Input.mousePosition); //right click
+			print("Player Position: " + GameObject.Find("playerCharacter").transform.position); //get player sphere position
+			//spawn particle at player sphere
+			bullet = Instantiate(Resources.Load("prefabAmmo"), new Vector3(1,2,3), Quaternion.identity) as GameObject;
+			//calculate path for particle
+			//spawn particle along path
 		}
 	}
 }
